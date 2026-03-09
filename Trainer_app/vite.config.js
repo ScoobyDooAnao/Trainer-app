3b76dcdaa9859eb3f8c061f96ade36125b29860b
ScoobyDooAnao <zavaschi.joao@gmail.com>
2026-03-09 00:42:20 -0300

    Update vite.config.js

--- Trainer_app/vite.config.js
+++ Trainer_app/vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          recharts: ['recharts'],
+          react: ['react', 'react-dom'],
+        }
+      }
+    }
+  }
 })
