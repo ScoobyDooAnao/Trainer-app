b3ceab04e8d669790a901bff8ead91b471ab628c
ScoobyDooAnao <zavaschi.joao@gmail.com>
2026-03-10 09:06:27 -0300

    Update vite.config.js

--- Trainer_app/vite.config.js
+++ Trainer_app/vite.config.js
@@ -1,10 +1,6 @@
-// rebuild
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-  }
 })
